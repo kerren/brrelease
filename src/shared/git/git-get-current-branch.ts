294896c4a33417b00f4206b1556e2c75b8910771
Kerren Ortlepp <kerren@entrostat.com>
2024-12-22 17:28:07 +0200

    Ensure there is no whitespace in the stdout for current branch

--- src/shared/git/git-get-current-branch.ts
+++ src/shared/git/git-get-current-branch.ts
@@ -2,5 +2,7 @@ import { spawnCommand } from '../spawn-command.js';
 
 export async function gitGetCurrentBranch(gitBinary: string) {
     // See https://stackoverflow.com/a/1418022
-    return spawnCommand(gitBinary, ['rev-parse', '--abbrev-ref', 'HEAD']);
+    const result = await spawnCommand(gitBinary, ['rev-parse', '--abbrev-ref', 'HEAD']);
+    result.stdout = result.stdout.split('\n')[0].trim();
+    return result;
 }
