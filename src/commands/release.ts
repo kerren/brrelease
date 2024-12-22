b7b5f0d1e870c8d0d22f9cc408f5835e67efb09a
Kerren Ortlepp <kerren@entrostat.com>
2024-12-22 14:41:59 +0200

    Add the tag prefix

--- src/commands/release.ts
+++ src/commands/release.ts
@@ -2,6 +2,7 @@ import { Args, Command, Flags } from '@oclif/core';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import commitAndTagVersion from 'commit-and-tag-version';
+import chalk from 'chalk';
 
 export default class Release extends Command {
     static override args = {};
@@ -10,14 +11,23 @@ export default class Release extends Command {
 
     static override examples = ['<%= config.bin %> <%= command.id %>'];
 
-    static override flags = {};
+    static override flags = {
+        'tag-prefix': Flags.string({
+            char: 'P',
+            description: 'The prefix that should be before the number on the tag made in git (default is "v")',
+            default: 'v',
+        }),
+    };
 
     public async run(): Promise<void> {
         const { args, flags } = await this.parse(Release);
 
+        const tagPrefix = flags['tag-prefix'] as string;
+
         try {
             const newVersion: string = await commitAndTagVersion({ dryRun: true, silent: true });
-            this.log(`The new release version will be ${newVersion}`);
+            const newVersionWithPrefix = `${tagPrefix}${newVersion}`;
+            this.log(`The new release version will be ${chalk.green(newVersionWithPrefix)}`);
         } catch (error) {
             this.error((error as any).message ?? (error as any));
         }
