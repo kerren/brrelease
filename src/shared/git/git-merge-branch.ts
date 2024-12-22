707c7533a4b8cd3c3dcccc6e23189e50b85d456e
Kerren Ortlepp <kerren@entrostat.com>
2024-12-22 19:55:12 +0200

    feat(git): Add the --no-ff flag to the branch merges

--- src/shared/git/git-merge-branch.ts
+++ src/shared/git/git-merge-branch.ts
@@ -1,9 +1,12 @@
 import { spawnCommand } from '../spawn-command.js';
 
-export async function gitMergeBranch(gitBinary: string, branchName: string, sign: boolean) {
+export async function gitMergeBranch(gitBinary: string, branchName: string, sign: boolean, noFastForward = true) {
     const commandArgs = ['merge', branchName];
     if (!sign) {
         commandArgs.push('--no-gpg-sign');
+    }
+    if (noFastForward) {
+        commandArgs.push('--no-ff');
     }
     return spawnCommand(gitBinary, commandArgs);
 }
